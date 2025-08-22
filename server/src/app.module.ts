c048e4b013e87cd8ab72042192e99345daac42b8
coder7475 <robiulhossain7475@gmail.com>
2025-08-22 23:20:39 +0600

    refactor: remove unused Users and Shops modules, controllers, and services to streamline application structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './database/prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { ShopsModule } from './shops/shops.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule,
-    UsersModule,
-    ShopsModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- server/src/shops/shops.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShopsService } from './shops.service';
-import { ShopsController } from './shops.controller';
-
-@Module({
-  providers: [ShopsService],
-  controllers: [ShopsController],
-})
-export class ShopsModule {}
