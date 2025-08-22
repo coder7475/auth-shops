701aad9fe743df87bb8560b18d340b816e351293
coder7475 <robiulhossain7475@gmail.com>
2025-08-22 22:51:50 +0600

    feat: implement signin functionality, and enhance authentication service with JWT support

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,12 +1,23 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
-import { SignupDto } from './dto/auth.dto';
+import { SigninDto, SignupDto } from './dto/auth.dto';
 import bcrypt from 'bcrypt';
+import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+  // signUp - Register
   async signup(dto: SignupDto) {
     const { user_name, password, shopNames } = dto;
 
@@ -48,5 +59,38 @@ export class AuthService {
       }
       throw err;
     }
+  }
+
+  async signin(dto: SigninDto, res: Response) {
+    const { user_name, password, rememberMe } = dto;
+
+    const user = await this.prisma.user.findUnique({
+      where: { user_name },
+      include: { shops: true },
+    });
+
+    if (!user) throw new NotFoundException('User not found!');
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) throw new UnauthorizedException('Incorrect password!');
+
+    const payload = { sub: user.user_id, username: user.user_name };
+
+    const token = await this.jwtService.signAsync(payload);
+
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      // * TODO: Must be available after frontend is finished
+      //   domain: process.env.COOKIE_DOMAIN,
+      maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 30 * 60 * 1000,
+    });
+
+    const data = {
+      userName: user.user_name,
+    };
+
+    return { message: 'Login successful!', data };
   }
 }
