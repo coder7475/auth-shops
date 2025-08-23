d76b5a3eb65a14ec0259bcecc1047cf2e74bfa51
coder7475 <robiulhossain7475@gmail.com>
2025-08-23 11:37:39 +0600

    feat: integrate Navbar and Footer components into CommonLayout and update user info handling in Home component

--- client/src/components/layout/CommonLayout.tsx
+++ client/src/components/layout/CommonLayout.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from "react";
 
 import ScrollToTop from "../ScrollToTop";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 interface IProps {
   children: ReactNode;
@@ -10,8 +12,9 @@ export default function CommonLayout({ children }: Readonly<IProps>) {
   return (
     <div className="flex min-h-screen flex-col">
       <ScrollToTop />
-
+      <Navbar />
       <div className="grow-1">{children}</div>
+      <Footer />
     </div>
   );
 }
