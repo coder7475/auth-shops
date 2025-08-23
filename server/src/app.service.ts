863e276046e9f88bcaf0f37b5271c706b97f7e59
coder7475 <robiulhossain7475@gmail.com>
2025-08-23 10:45:06 +0600

    refactor: update response structure in AppService and AuthService to include success status

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,7 +3,7 @@ import { IResponse } from './common/interfaces/response.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): IResponse<Promise<void>> {
+  getHello(): IResponse<null> {
     return {
       success: true,
       message: 'Shops backend is running!',
