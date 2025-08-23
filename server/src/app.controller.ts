449ca9b4fd71ec0e6d2aae8c6c3971bb54815f4e
coder7475 <robiulhossain7475@gmail.com>
2025-08-23 10:47:31 +0600

    refactor: update return type of getHello method in AppController to reflect new response structure

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): IResponse<Promise<void>> {
+  getHello(): IResponse<null> {
     return this.appService.getHello();
   }
 }
